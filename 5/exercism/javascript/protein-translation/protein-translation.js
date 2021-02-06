3214118de96bf4bef26971d7a61673cd9f6ae5a6
broop <soundwanders@gmail.com>
2021-02-06 17:35:42 -0500

    protein translation

---
+++ 5/exercism/javascript/protein-translation/protein-translation.js
@@ -0,0 +1,31 @@
+//
+// This is only a SKELETON file for the 'Protein Translation' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export const translate = (codons) => {
+  for (i = 0; i < 3; i++) {
+
+  }
+};
+
+
+export const codons = {
+  'AUG': Methionine,
+  'UUU': Phenylalanine,
+  'UUC': Phenylalanine,
+  'UUA': Leucine,
+  'UUG': Leucine,
+  'UCU': Serine,
+  'UCC': Serine,
+  'UCA': Serine,
+  'UCG': Serine,
+  'UAU': Tyrosine,
+  'UAC': Tyrosine,
+  'UGU': Cysteine,
+  'UGC': Cysteine,
+  'UGG': Tryptophan,
+  'UAA': STOP,
+  'UAG': STOP,
+  'UGA': STOP
+};
