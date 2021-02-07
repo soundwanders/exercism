b2e611e95d655a76f927b2a1aea5af17ee4bc539
broop <soundwanders@gmail.com>
2021-02-06 22:59:45 -0500

    protein translation completed

--- 5/exercism/javascript/protein-translation/protein-translation.js
+++ 5/exercism/javascript/protein-translation/protein-translation.js
@@ -1,31 +1,28 @@
-//
-// This is only a SKELETON file for the 'Protein Translation' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
-export const translate = (codons) => {
-  for (i = 0; i < 3; i++) {
-
-  }
+const codons = {
+  "AUG": "Methionine",
+  "UUU": "Phenylalanine",
+  "UUC": "Phenylalanine",
+  "UUA": "Leucine",
+  "UUG": "Leucine",
+  "UCU": "Serine",
+  "UCC": "Serine",
+  "UCA": "Serine",
+  "UCG": "Serine",
+  "UAU": "Tyrosine",
+  "UAC": "Tyrosine",
+  "UGU": "Cysteine",
+  "UGC": "Cysteine",
+  "UGG": "Tryptophan",
+  "UAA": "STOP",
+  "UAG": "STOP",
+  "UGA": "STOP",
 };
 
+export const translate = (RNA = '') => {
+  const protein = codons[RNA.slice(0,3)];
+  if (!RNA.length | protein === 'STOP')
+  return [];
 
-export const codons = {
-  'AUG': Methionine,
-  'UUU': Phenylalanine,
-  'UUC': Phenylalanine,
-  'UUA': Leucine,
-  'UUG': Leucine,
-  'UCU': Serine,
-  'UCC': Serine,
-  'UCA': Serine,
-  'UCG': Serine,
-  'UAU': Tyrosine,
-  'UAC': Tyrosine,
-  'UGU': Cysteine,
-  'UGC': Cysteine,
-  'UGG': Tryptophan,
-  'UAA': STOP,
-  'UAG': STOP,
-  'UGA': STOP
+  if (!protein) throw new Error('Invalid codon');
+  return [protein, ...translate(RNA.slice(3))];
 };
