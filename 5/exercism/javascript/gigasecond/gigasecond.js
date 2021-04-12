d96270470cc9008e23bdbb5df00c5bb9416e444d
soundwanders <soundwanders@gmail.com>
2021-04-11 22:06:46 -0400

    gigasecond solution

--- 5/exercism/javascript/gigasecond/gigasecond.js
+++ 5/exercism/javascript/gigasecond/gigasecond.js
@@ -1,7 +1,8 @@
 // return the future moment in time "d" by using Date constructor to identify the current moment in time "m"
 // and calculate a new date with one gigasecond added.
 
-export const gigasecond = (m) => {
-  let d = new Date(Number(m) + 10 ** 12);
-  return d;
+export const gigasecond = (currentMoment) => {
+  let oneGigasecond = 10 ** 12;
+  let futureMoment = new Date(currentMoment.getTime() + oneGigasecond);
+  return futureMoment;
 };
